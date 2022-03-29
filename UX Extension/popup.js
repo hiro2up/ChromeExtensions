f9e2ef75fd438d40f99280487bba1758ad05e921
Fabricio Nascimento <67124283+hiro2up@users.noreply.github.com>
2022-03-29 17:02:35 +0100

    Update
    Added switches and custom field fully functional.

--- UX Extension/popup.js
+++ UX Extension/popup.js
@@ -1,30 +1,30 @@
 // When the button is clicked, inject forceBolt into current page
-boltLive.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// boltLive.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: boltLiveFunc(tab),
-    });
-  });
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: boltLiveFunc(tab),
+//     });
+//   });
 
-tbLive.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// tbLive.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: tbLiveFunc(tab),
-    });
-  });
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: tbLiveFunc(tab),
+//     });
+//   });
 
-nameButton.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// nameButton.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: nameFunc(tab),
-    });
-  });
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: nameFunc(tab),
+//     });
+//   });
 
 // Submiting choices in from the switches
 submitChoices.addEventListener("click", async () => {
@@ -40,65 +40,65 @@ submitChoices.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 
-function boltLiveFunc(tab){
-    var customParam = encodeURI('forceBolt');
-    var url = tab.url;
+// function boltLiveFunc(tab){
+//     var customParam = encodeURI('forceBolt');
+//     var url = tab.url;
 
-    if (url.indexOf(customParam) === -1){
-        var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
-        var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
-        var newUrl = url.substring(0, hashStart) + querySymbol + customParam +
-                    url.substring(hashStart);
+//     if (url.indexOf(customParam) === -1){
+//         var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
+//         var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
+//         var newUrl = url.substring(0, hashStart) + querySymbol + customParam +
+//                     url.substring(hashStart);
 
-        chrome.tabs.update(tab.id, {url: newUrl});
-    } else {
-        var newUrl = url;
-        chrome.tabs.update(tab.id, {url: newUrl});
-    }
-}
+//         chrome.tabs.update(tab.id, {url: newUrl});
+//     } else {
+//         var newUrl = url;
+//         chrome.tabs.update(tab.id, {url: newUrl});
+//     }
+// }
 
 
-function tbLiveFunc(tab){
-    var customParam = encodeURI('forceBolt');
-    var url = tab.url;
+// function tbLiveFunc(tab){
+//     var customParam = encodeURI('forceBolt');
+//     var url = tab.url;
 
-    if (url.indexOf(customParam) === -1){
-        var newUrl = url;
-        chrome.tabs.update(tab.id, {url: newUrl});
-    } else {
-        if (url.indexOf(customParam+'&') === -1){
-            var newUrl = url.substring(0,url.indexOf(customParam)-1);
-            chrome.tabs.update(tab.id, {url: newUrl});
-        }
-    }
-}
+//     if (url.indexOf(customParam) === -1){
+//         var newUrl = url;
+//         chrome.tabs.update(tab.id, {url: newUrl});
+//     } else {
+//         if (url.indexOf(customParam+'&') === -1){
+//             var newUrl = url.substring(0,url.indexOf(customParam)-1);
+//             chrome.tabs.update(tab.id, {url: newUrl});
+//         }
+//     }
+// }
 
 
 // Functions for table columns
-function nameFunc(tab){
-    var customParam = encodeURI('name');
-    var url = tab.url;
+// function nameFunc(tab){
+//     var customParam = encodeURI('name');
+//     var url = tab.url;
 
-    if(url.indexOf(','+customParam) === -1 && url.indexOf('%3D'+customParam) === -1){
-        var newUrl = url.substring(0,url.indexOf('&fields')+8) + customParam + url.substring(url.indexOf('&levels'))
-        chrome.tabs.update(tab.id, {url: newUrl});
+//     if(url.indexOf(','+customParam) === -1 && url.indexOf('%3D'+customParam) === -1){
+//         var newUrl = url.substring(0,url.indexOf('&fields')+8) + customParam + url.substring(url.indexOf('&levels'))
+//         chrome.tabs.update(tab.id, {url: newUrl});
         
-        // chrome.tabs.onUpdated.addListener(
-        //   tab.id, {url: newUrl}
-        // );
-        // chrome.tabs.reload();
+//         // chrome.tabs.onUpdated.addListener(
+//         //   tab.id, {url: newUrl}
+//         // );
+//         // chrome.tabs.reload();
 
-        // window.addEventListener('keydown', (e) => {
-        //   console.log(e)
-        // })
+//         // window.addEventListener('keydown', (e) => {
+//         //   console.log(e)
+//         // })
 
         
         
-        // window.dispatchEvent(new KeyboardEvent('keydown', {
-        //   'key': 'F5'
-        // }));
-    }
-}
+//         // window.dispatchEvent(new KeyboardEvent('keydown', {
+//         //   'key': 'F5'
+//         // }));
+//     }
+// }
 
 
 // Collecting the switch values
@@ -111,8 +111,19 @@ function fieldChoices(tab){
 
   var customParam = encodeURI('name');
   var url = tab.url;
-  Array.from(form.elements).forEach(element => {
-    var newUrl = url + element
+  const form = document.getElementById('fieldChoices').elements;
+  let choices = [];
+  Array.from(form).forEach(element => {
+    if (element.checked && element.type === 'checkbox'){
+      // console.log(element.id.substring(0,element.id.length-6));
+      choices.push(element.id.substring(0,element.id.length-6));
+    }
+    if (element.value !== '' && element.type === 'text')  {
+      choices.push(element.value);
+    }
   });
-  chrome.tabs.update(tab.id, {url: newUrl});
+  if (choices.length > 0) {
+    var newUrl = url.substring(0,url.indexOf('&fields')+8) + choices.join(',') + url.substring(url.indexOf('&levels'))
+    chrome.tabs.update(tab.id, {url: newUrl});
+  }
 }
